7af1481ff20c850f026dd306a41673efaacd173c
Mohammed <mdfaridh142002@gmail.com>
2026-02-17 18:28:47 +0530

    Add Folder with files

--- Basic Clauses/DATA MANIPULATION LANGUAGE (DML).sql
+++ Basic Clauses/DATA MANIPULATION LANGUAGE (DML).sql
@@ -5,7 +5,9 @@
 	--> DELETE
 */
 
---THIS WILL CREATE DATA IN THE TABLE DDL
+--THIS WILL CREATE DATA IN THE TABLE DDL 
+
+									--MANUAL INSERTION--
 INSERT INTO DDL (ID, NAME, AGE, State)
 VALUES (1, 'Abitha', '45', 'Tamil Nadu'),
 	   (2, 'khalil', '55', 'Tamil Nadu'),
@@ -13,7 +15,22 @@ VALUES (1, 'Abitha', '45', 'Tamil Nadu'),
 	   (4, 'Faridh', '23', 'Tamil Nadu'),
 	   (5, 'Javidh', '23', 'Karnataka')
 
--- THIS WILL UPDATE THE DATA IN THE TABLE DDL
+-- This will insert the whole table into an another table using "SELECT" (SOURCE) ->(TRAGET)
+-- NOTE: Ensure CREATION of Target Table 
+
+								--INSERTION USNIG SELECT
+INSERT INTO TEMP (ID, NAME, AGE, CITY)
+SELECT 
+	ID,
+	NAME,
+	AGE,
+	NULL AS CITY
+FROM DDL
+
+
+-- THIS WILL DELETE THE ENTIRE ROW FROM THE TABLE DDL
+-- IF 'WHERE' IS NOT GIVEN IT WILL DELETE ALL THE ROWS FROM THE TABLE
+
 UPDATE DDL
 	set AGE = 22
 where 
@@ -21,4 +38,8 @@ where
 
 DELETE FROM DDL
 where id = 5
+
+--THIS WILL DELETE THE ENTIRE DATA FROM THE TABLE WITHOUT ANY LOGGING (MUCH FASTER).
+--CAN'T USE 'WHERE' HERE.
+TRUNCATE TABLE DDL
 

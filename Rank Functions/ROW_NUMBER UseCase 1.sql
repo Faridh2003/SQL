c3b8a1d1eb6fd0b1940003d2cadeb8226cb7853c
Mohammed <mdfaridh142002@gmail.com>
2026-04-07 18:56:45 +0530

    All Use Cases

--- Rank Functions/ROW_NUMBER UseCase 1.sql
+++ Rank Functions/ROW_NUMBER UseCase 1.sql
@@ -9,3 +9,41 @@ OrderDate,
 Sales,
 ROW_NUMBER() OVER( PARTITION BY ProductID ORDER BY Sales DESC) AS ProductRanks
 FROM Sales.Orders
+
+
+
+-- Bottom-N-Analysis
+
+-- Find the bottom lowest 2 customers based on total sales
+
+
+SELECT
+*
+FROM(
+	SELECT
+		CustomerID,
+		SUM(Sales) AS Total_Sales,
+		ROW_NUMBER() OVER(ORDER BY SUM(Sales)) AS TotalSales
+	FROM sales.Orders
+	GROUP BY CustomerID
+)t WHERE TotalSales <= 2
+
+
+
+-- Generate Unique ID's
+
+-- Assign unique ID's to the rows of "Orders Archive" table
+
+SELECT
+ROW_NUMBER() OVER(ORDER BY OrderID, OrderDate) AS UniqueID,
+*
+FROM Sales.OrdersArchive
+
+
+-- Identify Duplicates
+
+-- Identify duplicate rows in the table "Orders Archive" and return thr clrean result without duplicates
+
+SELECT
+*
+FROM sales.OrdersArchive
